3930a44f95d467837488fcbf613a08dc548c4453
Multicast96 <kamil.dakus@gmail.com>
2018-02-28 17:48:50 +0100

    Simple doubly-linked-list in C

---
+++ C/doubly-linked-list/doubly-linked-list.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+typedef struct{
+	struct element* next;
+	int key;
+	struct element *previous;
+}element;
+
+
+void print_list(element *head) {
+	head = head->next;
+	while (head != NULL){
+		printf("%d->", head->key);
+		head = head->next;
+	}
+	printf("NULL\n");
+}
+
+void push_front(element *head, int key) {
+	element *tmp = (element*)malloc(sizeof(element));
+	tmp->key = key;
+	tmp->previous = head;
+	tmp->next = head->next;
+	
+	if(head->next != NULL) ((element *)head->next)->previous = tmp;
+	head->next = tmp;
+	
+	head->key++; //num of elements in list
+}
+
+void push_back(element *head, int key) {
+	element *tmp = (element*)malloc(sizeof(element));
+	tmp->key = key;
+	tmp->next = NULL;
+
+	while (head->next != NULL) head = head->next;
+
+	head->next = tmp;
+	tmp->previous = head;
+}
+
+void remove_key(element *head, int key) {
+	element *tmp = head->next;
+
+	while (tmp != NULL){
+		if (tmp->key == key) {
+			if (tmp->previous != NULL) ((element *)tmp->previous)->next = tmp->next;
+			if (tmp->next != NULL) ((element *)tmp->next)->previous = tmp->previous;
+			
+			free(tmp);
+			return;
+		}
+		tmp = tmp->next;
+	}
+}
+
+int main() {
+	element list = { NULL, 0 , NULL };
+	push_front(&list, 10);
+	push_front(&list, 20);
+	push_front(&list, 30);
+	push_back(&list, 5);
+	push_back(&list, 1);
+	print_list(&list);
+	remove_key(&list, 30);
+	print_list(&list);
+	remove_key(&list, 1);
+	print_list(&list);
+	remove_key(&list, 10);
+	print_list(&list);
+	return 0;
+}
